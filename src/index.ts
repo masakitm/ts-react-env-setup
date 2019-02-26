2432aae8056fe8f7fe07c4787b01283bdbb8de4f
友塚　正樹 <uu144692@CA4167.local>
2019-02-26 18:23:24 +0900

    feature: react

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-const a = {
-  a: 1,
-  b: 1
-};
-
-console.log({ ...a, b: 2 });
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const root = document.getElementById("root");
+
+const App = () => {
+  return <div>Hello, Hello, Hello!</div>;
+};
+
+ReactDOM.render(<App />, root);
